7018cb1e1c338f19c3f5362b46e2de72bc89b504
Taylor Pokoj <pokojt@gmail.com>
2016-03-18 13:35:11 -0700

    Clean up code, make each search prepend instead of append

--- js/user-search.js
+++ js/user-search.js
@@ -1,16 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.UserSearch = function(username, repos, descriptions) {
+exports.UserSearch = function(username) {
   this.username = username;
-  this.repos = repos;
-  this.descriptions = descriptions;
 };
 
 exports.UserSearch.prototype.getSummary = function() {
   $.get('https://api.github.com/users/'+this.username+'?access_token=' + apiKey).then(function(response){
-    console.log(response);
 
-    $(".results").append('<div class="searchItem">'+
+    $(".results").prepend('<div class="searchItem">'+
                             '<div class="userHeader">' +
                               '<img class="userPhoto" src="'+response.avatar_url+'">'+
                               '<div class="userText">'+
@@ -30,14 +27,11 @@ exports.UserSearch.prototype.getSummary = function() {
 
 exports.UserSearch.prototype.getRepos = function() {
   $.get('https://api.github.com/users/'+this.username+'/repos?access_token=' + apiKey + '&per_page=1000&sort=update').then(function(response){
-    console.log(response);
 
     for(var i=0; i < response.length; i++) {
 
       var projectName = response[i].name;
       var projectDescription = response[i].description;
-
-      console.log(projectName, projectDescription);
 
       $(".repos").append('<div class="repoItem">' +
                             '<a target="_blank" href="https://github.com/'+response[0].owner.login+'/'+projectName+'"><h5>'+projectName+'</h5></a>' +
@@ -47,15 +41,4 @@ exports.UserSearch.prototype.getRepos = function() {
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
-
-
-
-
-
-
-
-
-
-
-
 };
