5dde3e8bb5267df964fd86e1979f7f4e025d25ce
Taylor Pokoj <pokojt@gmail.com>
2016-03-18 08:08:23 -0700

    Setup initial files for project

---
+++ js/browser-interface.js
@@ -0,0 +1,13 @@
+// var Constructor = require('../js/function-name.js').Constructor;
+//
+// $(document).ready(function() {
+//
+// var newConstructor = new Constructor();
+//
+//
+// });
+
+var apiKey = require('./../.env').apiKey;
+
+
+/* Front-end User interface (jquery) code goes in this file */
